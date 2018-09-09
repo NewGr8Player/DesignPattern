f9e01b40666b7374192ef2225c3759b963d1a2b4
NewGr8Player <273221594@qq.com>
2018-09-09 19:00:29 +0800

    Add     - 结构型设计模式基本目录

---
+++ src/main/java/com/xavier/pattern/structural/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * 结构型模式(Structural Patterns)
+ * <ul>
+ * <li>适配器模式(The Adapter Pattern)</li>
+ * <li>组合模式(The Composite Pattern)</li>
+ * <li>装饰者(The Decorator Pattern)</li>
+ * <li>外观模式(The Facade Pattern)</li>
+ * <li>代理模式(The Proxy Pattern)</li>
+ * <li>桥接模式 (The Bridge Pattern)</li>
+ * <li>享元模式 (The Flyweight Pattern)</li>
+ * </ul>
+ *
+ * @author NewGr8Player
+ */
+package com.xavier.pattern.structural;
+
+
