02e4a24ef9d39b5b8bd14067b64c3abd7c63d9e4
NewGr8Player <273221594@qq.com>
2018-10-31 13:27:59 +0800

    Add     - 访问者模式 The Visitor Pattern     - 备忘录模式 The Memento Pattern

--- src/test/java/com/xavier/MainTestCase.java
+++ src/test/java/com/xavier/MainTestCase.java
@@ -1,14 +1,9 @@
 package com.xavier;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class MainTestCase {
 
-    @Test
-    public void test() throws InterruptedException {
-        System.out.println("a");
-    }
 }
